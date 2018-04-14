9dd8ad0b9f74b69ac5426c7dbeca042177868db5
Raurur00 <javierzavalatorres@gmail.com>
2018-04-14 14:55:53 -0300

    Bullets

---
+++ NinjaShooter2D/Assets/Scripts/PalyerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalyerController : MonoBehaviour {
+	public GameObject rightBullet;
+	Transform firePos;
+
+	// Use this for initialization
+	void Start () {
+		firePos = transform.Find("firePos");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Fire();
+		}
+	}
+	void Fire(){
+		Instantiate(rightBullet,firePos.position,Quaternion.identity);
+	}
+}
