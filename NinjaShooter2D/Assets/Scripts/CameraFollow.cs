7fd61ecc0cd8786ea23fadbb9e2691cca31789e0
pathdll <pcampaa@gmail.com>
2018-04-14 14:53:51 -0300

    smooth camera fix

--- NinjaShooter2D/Assets/Scripts/CameraFollow.cs
+++ NinjaShooter2D/Assets/Scripts/CameraFollow.cs
@@ -3,13 +3,13 @@
 public class CameraFollow : MonoBehaviour {
 	public Transform target;
 	public float smoothSpeed = 0.125f;
-	public Vector3 offset;
+	public Vector2 offset;
 
 	void LateUpdate()
 	{
-		Vector3 pos = target.position + offset;
-		Vector3 smoothedPos = Vector3.Lerp(target.position, pos, smoothSpeed);
-		transform.position =  smoothedPos;
+		Vector2 targetPosition = new Vector2(target.position.x, target.position.y);
+		Vector3 smoothedPos = Vector2.Lerp(targetPosition, new Vector3(transform.position.x, transform.position.y), smoothSpeed);
+		transform.position =  new Vector3(smoothedPos.x, smoothedPos.y, -1);
 	}
 
 }
