9a86d6bf1832f870a1b0de7a7418b65fab77fa4a
John Rodríguez <Jneror@users.noreply.github.com>
2018-04-14 13:12:27 -0300

    GGEZ3

--- NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
+++ NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	public float playerVelocity = 1.5f;
+	public float playerVelocity = 3f;
+	public float acceleration = 9.8f;
+	public float friction = 10f;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,10 +15,10 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 		Vector3 move = Vector3.zero;
 		if (Input.GetAxis("Horizontal") != 0)
-			move += Vector3.right*Input.GetAxisRaw("Horizontal")*playerVelocity*Time.deltaTime*2;
+			move += Vector3.right*Input.GetAxisRaw("Horizontal")*playerVelocity*Time.deltaTime;
 		
 		if (Input.GetAxis("Vertical") != 0)
-			move += Vector3.up*Input.GetAxisRaw("Vertical")*playerVelocity*Time.deltaTime*2;
+			move += Vector3.up*Input.GetAxisRaw("Vertical")*playerVelocity*Time.deltaTime;
 
 		transform.Translate(move);
 	}
