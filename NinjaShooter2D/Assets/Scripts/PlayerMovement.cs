666c3c095fb5f60b708a69770ff27d0f73c180e6
John Rodríguez <Jneror@users.noreply.github.com>
2018-04-14 13:41:43 -0300

    Move

--- NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
+++ NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,22 @@ public class PlayerMovement : MonoBehaviour {
 	public float playerVelocity = 3f;
 	public float acceleration = 9.8f;
 	public float friction = 10f;
+	private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () {
-		Vector3 move = Vector3.zero;
+		Vector3 accel = Vector3.zero;
 		if (Input.GetAxis("Horizontal") != 0)
-			move += Vector3.right*Input.GetAxisRaw("Horizontal")*playerVelocity*Time.deltaTime;
+			accel += acceleration*Vector3.right*Input.GetAxisRaw("Horizontal");
 		
 		if (Input.GetAxis("Vertical") != 0)
-			move += Vector3.up*Input.GetAxisRaw("Vertical")*playerVelocity*Time.deltaTime;
+			accel += acceleration*Vector3.up*Input.GetAxisRaw("Vertical");
 
-		transform.Translate(move);
+		accel = accel.normalized*acceleration;
+		rb.AddForce(accel);
+		Debug.Log(rb.velocity.magnitude);
 	}
 }
