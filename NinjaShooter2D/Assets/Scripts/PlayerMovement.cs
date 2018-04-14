9fb57f6ea5e500e34c0fbe72abe4497014e382bc
John Rodríguez <Jneror@users.noreply.github.com>
2018-04-14 13:02:15 -0300

    GGEZ2

--- NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
+++ NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
@@ -12,9 +12,12 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 move = Vector3.zero;
-		if (Input.GetAxis("Horizontal") != 0) {
-			move += Vector3.right*Input.GetAxisRaw("Horizontal")*playerVelocity*Time.deltaTime;
-		}
+		if (Input.GetAxis("Horizontal") != 0)
+			move += Vector3.right*Input.GetAxisRaw("Horizontal")*playerVelocity*Time.deltaTime*2;
+		
+		if (Input.GetAxis("Vertical") != 0)
+			move += Vector3.up*Input.GetAxisRaw("Vertical")*playerVelocity*Time.deltaTime*2;
+
 		transform.Translate(move);
 	}
 }
