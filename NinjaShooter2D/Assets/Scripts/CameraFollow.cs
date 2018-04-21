d181fb60884d8cedb72eebb592cd5dc004601365
pathdll <pcampaa@gmail.com>
2018-04-21 11:48:06 -0300

    optimized movement

--- NinjaShooter2D/Assets/Scripts/CameraFollow.cs
+++ NinjaShooter2D/Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,24 @@
 ﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-	public GameObject player;
-    public Transform target;
+	public Transform target;
+    [Range(0, 1)]
+    public float smoothFactor = 0.3f;
     private Vector3 offset;
 
     void Start () 
     {
-        offset = transform.position - player.transform.position;
+        offset = transform.position - target.position;
     }
     void LateUpdate () 
     {
-        transform.position = player.transform.position + offset;
+        Vector2 originPosition = Vector2.right * transform.position.x + Vector2.up * transform.position.y;
+        Vector2 focusPosition = Vector2.right * target.position.x + Vector2.up * target.position.y;
+        Vector2 finalPosition = Vector2.Lerp(originPosition, focusPosition, smoothFactor);
+
+        Debug.Log(focusPosition);
+
+        transform.position = new Vector3(finalPosition.x, finalPosition.y, transform.position.z);
     }
 
 }
--- NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
+++ NinjaShooter2D/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	public float playerVelocity = 2f;
+	public float dashVelIncrease = 0.4f;
 	//private Rigidbody2D rb;
-	
+
 	void Start () {
 		//rb = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () {
 
-		Vector3 mov = Vector3.zero;
-		if (Input.GetAxis("Horizontal") != 0)
-			mov += Vector3.right*Input.GetAxisRaw("Horizontal");
-		
-		if (Input.GetAxis("Vertical") != 0)
-			mov += Vector3.up*Input.GetAxisRaw("Vertical");
-		
-		mov = mov.normalized*playerVelocity*Time.deltaTime;
-		transform.Translate(mov);
+		bool movedHorizontal = Input.GetAxis("Horizontal") != 0;
+		bool movedVertical = Input.GetAxis("Vertical") != 0;
 
-
+		if (movedHorizontal || movedVertical)
+		{
+			Vector3 mov = Vector3.zero;
+			if (movedHorizontal)
+				mov += Vector3.right * Input.GetAxisRaw("Horizontal");
+			
+			if (movedVertical)
+				mov += Vector3.up * Input.GetAxisRaw("Vertical");
+			
+			mov = mov.normalized * playerVelocity * Time.deltaTime;
+			transform.Translate(mov);
+		}
 	}
 }
