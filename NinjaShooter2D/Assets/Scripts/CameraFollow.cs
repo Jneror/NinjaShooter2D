10cb4a2fc8649d74be8a4deff6e9557332a54823
Raurur00 <javierzavalatorres@gmail.com>
2018-05-05 13:08:31 -0300

    Camera Follow

---
+++ NinjaShooter2D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	private Transform target;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = new Vector3(target.position.x,target.position.y,transform.position.z);	
+	}
+}
